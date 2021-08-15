356643ed3df34986a73a9e11921e5719d63e47f1
Korsun Pavel <45067434+kcvpll@users.noreply.github.com>
2021-08-15 04:01:21 +0300

    Revert "Изменения в проекте"
    This reverts commit d26a24991a19ce7e42a5cb4785ea4e19c38fbd76.

--- app/src/main/java/com/kkkkorsun/testproject/api/Api.kt
+++ app/src/main/java/com/kkkkorsun/testproject/api/Api.kt
@@ -2,7 +2,6 @@ package com.kkkkorsun.testproject.api
 
 import android.telecom.Call
 import com.kkkkorsun.testproject.data.model.DetailUserResponse
-import com.kkkkorsun.testproject.data.model.User
 import com.kkkkorsun.testproject.data.model.UserResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -11,20 +10,15 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Autorization: token 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")
     fun getSearchUsers(
         @Query("q") query: String
     ): retrofit2.Call<UserResponse>
 
-    @GET("users/{username}")
-    @Headers("Authorization: token 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")
-    fun getUserDetail(
+    @GET("user/{username}")
+    @Headers("Autorization: token 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")
+   fun getUserDetail(
         @Path("username") username: String
     ): retrofit2.Call<DetailUserResponse>
 
-    @GET("users/{username}/repos")
-    @Headers("Authorization: token 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")
-    fun getRepos(
-        @Path("username") username: String
-    ): retrofit2.Call<ArrayList<User>>
 }
--- app/src/main/java/com/kkkkorsun/testproject/ui/main/UserAdapter.kt
+++ app/src/main/java/com/kkkkorsun/testproject/ui/main/UserAdapter.kt
@@ -12,12 +12,6 @@ class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
     private val list = ArrayList<User>()
 
-    private var onItemClickCallback: OnItemClickCallback? = null
-
-    fun setOnItemClickCallback (onItemClickCallback: OnItemClickCallback){
-        this.onItemClickCallback = onItemClickCallback
-    }
-
     fun setList(users: ArrayList<User>) {
         list.clear()
         list.addAll(users)
@@ -28,11 +22,6 @@ class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
         RecyclerView.ViewHolder(binding.root) {
         fun bind(user: User) {
             binding.apply {
-
-                binding.root.setOnClickListener {
-                    onItemClickCallback?.onItemClicked(user)
-                }
-
                 Glide.with(itemView)
                     .load(user.avatar_url)
                     .transition(DrawableTransitionOptions.withCrossFade())
@@ -56,8 +45,4 @@ class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
     }
 
     override fun getItemCount(): Int = list.size
-
-    interface OnItemClickCallback{
-        fun onItemClicked(data: User)
-    }
 }
